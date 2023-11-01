f1f8002341bbc49cc966dd59dcf5c9465a8c8869
matt-blac <bomettmat2@gmail.com>
2023-11-01 17:41:51 +0300

    partially completed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Task from "./Task";
+import { TASKS } from "../data";
 
 function TaskList() {
+  const [tasks, setTasks] = React.useState(TASKS);
+
+  const handleDelete = (text) => {
+    setTasks(tasks.filter((task) => task.text !== text));
+  };
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          text={task.text}
+          category={task.category}
+          onDelete={() => handleDelete(task.text)}
+        />
+      ))}
     </div>
   );
 }
